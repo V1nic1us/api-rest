b0db5b61b938812a9a043a95312c4984036ab536
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-05-01 18:11:07 -0400

    feat: added user model in connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
+import Users from '../models/User';
 
-const models = [Aluno];
+const models = [Aluno, Users];
 
 const conection = new Sequelize(databaseConfig);
 
