cfa69a73b2fd37f168adc0cd23b7f39360573766
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-05-15 21:59:35 -0400

    fix:removed controller function to create students

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'marcus',
-      sobrenome: 'marques',
-      email: 'teste@gmail.com',
-      idade: 18,
-      peso: 47.76,
-      altura: 1.65,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
