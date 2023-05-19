7d7654466848001354dedbd0364aa5fb9682948a
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-05-19 13:50:36 -0400

    feat: controller do file validation

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,14 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
-// const uploads= multer(multerConfig); salvar varios arquivos ao mesmo tempo
-// router.post('/', upload.fields('foto'), fotoController.store);
 
 const router = new Router();
-router.post('/', upload.single('foto'), fotoController.store);
+
+router.post('/', fotoController.store);
 
 export default router;
