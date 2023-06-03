d64bf187f0731d325b03e47f4995b6c1a3bd0acc
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-06-03 10:36:17 -0400

    feat: criado configuração do cors no middlewares

--- src/app.js
+++ src/app.js
@@ -6,11 +6,30 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  'http://',
+  'http:// localhost:3000',
+  'http:// localhost:3001',
+];
+
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (whiteList.indexOf(req.header('Origin') !== -1)) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
 
 class App {
   constructor() {
@@ -22,10 +41,12 @@ class App {
   middelewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
-    this.app.use(express.static('/imagens/', resolve(__dirname, '..', 'uploads', 'imagens')));
+    this.app.use(express.static(resolve(__dirname, '..', 'uploads', 'imagens')));
   }
 
   routes() {
+    this.app.use(cors(corsOptionsDelegate));
+    this.app.use(helmet());
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
